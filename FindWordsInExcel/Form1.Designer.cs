57402ae3a16bf2a4b9e6df72b3fadaa3bc030972
MaEunJa <55179810+MaEunJa@users.noreply.github.com>
2023-10-13 20:47:41 +0900

    d

---
+++ FindWordsInExcel/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace FindWordsInExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSrcFind = new System.Windows.Forms.Button();
+            this.textSrc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textKeyword = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textColum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblProcess = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSrcFind
+            // 
+            this.btnSrcFind.Location = new System.Drawing.Point(884, 46);
+            this.btnSrcFind.Name = "btnSrcFind";
+            this.btnSrcFind.Size = new System.Drawing.Size(97, 40);
+            this.btnSrcFind.TabIndex = 0;
+            this.btnSrcFind.Text = "Find";
+            this.btnSrcFind.UseVisualStyleBackColor = true;
+            this.btnSrcFind.Click += new System.EventHandler(this.btnSrcFind_Click);
+            // 
+            // textSrc
+            // 
+            this.textSrc.Location = new System.Drawing.Point(191, 46);
+            this.textSrc.Name = "textSrc";
+            this.textSrc.ReadOnly = true;
+            this.textSrc.Size = new System.Drawing.Size(673, 26);
+            this.textSrc.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Keyword";
+            // 
+            // textKeyword
+            // 
+            this.textKeyword.Location = new System.Drawing.Point(191, 100);
+            this.textKeyword.Name = "textKeyword";
+            this.textKeyword.Size = new System.Drawing.Size(673, 26);
+            this.textKeyword.TabIndex = 1;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(884, 100);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(97, 40);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // textResult
+            // 
+            this.textResult.Location = new System.Drawing.Point(77, 218);
+            this.textResult.Multiline = true;
+            this.textResult.Name = "textResult";
+            this.textResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textResult.Size = new System.Drawing.Size(1092, 391);
+            this.textResult.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Source Folder";
+            // 
+            // textColum
+            // 
+            this.textColum.Location = new System.Drawing.Point(191, 153);
+            this.textColum.Name = "textColum";
+            this.textColum.Size = new System.Drawing.Size(104, 26);
+            this.textColum.TabIndex = 1;
+            this.textColum.Text = "3";
+            this.textColum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Target Colum";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(78, 193);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1000, 13);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // lblProcess
+            // 
+            this.lblProcess.AutoSize = true;
+            this.lblProcess.Location = new System.Drawing.Point(1084, 188);
+            this.lblProcess.Name = "lblProcess";
+            this.lblProcess.Size = new System.Drawing.Size(0, 18);
+            this.lblProcess.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1197, 632);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblProcess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textColum);
+            this.Controls.Add(this.textKeyword);
+            this.Controls.Add(this.textSrc);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.btnSrcFind);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Find words in excel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSrcFind;
+        private System.Windows.Forms.TextBox textSrc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textKeyword;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textColum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblProcess;
+    }
+}
+
---
+++ FindWordsInExcel/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace FindWordsInExcel
+{
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker backgroundWorker;
+        public Form1()
+        {
+            InitializeComponent();
+
+            textSrc.Height = 38;
+            textSrc.AutoSize = false; // 자동 크기 조절 비활성화
+            textKeyword.Height = 38;
+            textKeyword.AutoSize = false;
+
+            // BackgroundWorker 초기화
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.DoWork += backgroundWorker_DoWork;
+            backgroundWorker.ProgressChanged += backgroundWorker_ProgressChanged;
+
+        }
+
+        private void btnSrcFind_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "폴더를 선택하세요.";
+
+            DialogResult result = folderBrowserDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string selectedFolder = folderBrowserDialog.SelectedPath;
+                //Console.WriteLine("선택한 폴더 경로: " + selectedFolder);
+                textSrc.Text = selectedFolder;
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textColum.Text))
+            {
+                MessageBox.Show("탬색할 컬럼을 입력해주세요.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(textKeyword.Text))
+            {
+                MessageBox.Show("탬색할 단어를 입력해주세요.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(textSrc.Text))
+            {
+                MessageBox.Show("탬색할 폴더를 선택해주세요.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            backgroundWorker.RunWorkerAsync();
+
+
+
+        }
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // 검색 작업을 백그라운드 스레드에서 수행
+            // progressBar1 값을 업데이트하여 진행 상황을 표시
+            int colNum = int.Parse(textColum.Text);
+            string result = "";
+            DirectoryInfo directory = new DirectoryInfo(textSrc.Text);
+            int completedCount = 0;
+            progressBar1.Value = 0;
+
+            int fileCount = 0;
+
+            foreach (FileInfo file in directory.GetFiles("*.xlsx"))
+            {
+                // 무시할 잠금 파일 처리
+                if (file.Name.StartsWith("~$")) continue;
+                fileCount++;
+            }
+
+                if (InvokeRequired)
+            {
+                // 현재 스레드가 UI 스레드가 아니라면, UI 스레드에서 실행하도록 호출
+                Invoke(new Action(() => lblProcess.Text = "0/" + fileCount));
+            }
+            else
+            {
+                // 현재 스레드가 이미 UI 스레드인 경우 바로 업데이트
+                lblProcess.Text = "0/" + fileCount;
+            }
+            if (directory.Exists)
+            {
+                foreach (FileInfo file in directory.GetFiles("*.xlsx"))
+                {
+                    // 무시할 잠금 파일 처리
+                    if (file.Name.StartsWith("~$")) continue;
+                    SearchWordInExcelFile(file.FullName, textKeyword.Text, colNum);
+                    completedCount++;
+                    // 진행 상황 업데이트
+                    int progress = (int)((double)completedCount / fileCount * 100);
+                    backgroundWorker.ReportProgress(progress);
+
+                    if (InvokeRequired)
+                    {
+                        // 현재 스레드가 UI 스레드가 아니라면, UI 스레드에서 실행하도록 호출
+                        Invoke(new Action(() => lblProcess.Text = completedCount+"/" + fileCount));
+                    }
+                    else
+                    {
+                        // 현재 스레드가 이미 UI 스레드인 경우 바로 업데이트
+                        lblProcess.Text = completedCount + "/" + fileCount;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("폴더를 찾을 수 없습니다.");
+            }
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // 백그라운드 스레드에서 보낸 진행 상황 값을 사용하여 ProgressBar 업데이트
+
+            if (InvokeRequired)
+            {
+                // 현재 스레드가 UI 스레드가 아니라면, UI 스레드에서 실행하도록 호출
+                Invoke(new Action(() => progressBar1.Value = e.ProgressPercentage));
+            }
+            else
+            {
+                // 현재 스레드가 이미 UI 스레드인 경우 바로 업데이트
+                progressBar1.Value = e.ProgressPercentage;
+            }
+        }
+
+
+        public string SearchWordInExcelFile(string filePath, string searchWord,int colNum)
+        {
+            string result = "";
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook workbook = excelApp.Workbooks.Open(filePath);
+            Excel.Worksheet worksheet = workbook.Sheets[1];
+            Excel.Range range = worksheet.UsedRange;
+
+            foreach (Excel.Range cell in range.Cells)
+            {
+                if (cell.Column == colNum && cell.Value != null && cell.Value.ToString().IndexOf(searchWord, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    Console.WriteLine($"파일: {Path.GetFileName(filePath)}, 찾은 위치: 행 {cell.Row}, 열 {cell.Column}");
+
+                    if (InvokeRequired)
+                    {
+                        // 현재 스레드가 UI 스레드가 아니라면, UI 스레드에서 실행하도록 호출
+                        Invoke(new Action(() =>
+                        {
+                            textResult.Text += $"파일: {Path.GetFileName(filePath)}, 찾은 위치: 행 {cell.Row}, 열 {cell.Column}\r\n";
+                            textResult.SelectionStart = textResult.Text.Length; // 커서를 텍스트 끝으로 이동
+                            textResult.ScrollToCaret(); // 스크롤을 커서 위치로 이동
+                        }
+                        ));
+                    }
+                    else
+                    {
+                        // 현재 스레드가 이미 UI 스레드인 경우 바로 업데이트
+                        textResult.Text += $"파일: {Path.GetFileName(filePath)}, 찾은 위치: 행 {cell.Row}, 열 {cell.Column}\r\n";
+                        textResult.SelectionStart = textResult.Text.Length; // 커서를 텍스트 끝으로 이동
+                        textResult.ScrollToCaret(); // 스크롤을 커서 위치로 이동
+                    }
+
+
+
+                }
+            }
+
+            Marshal.ReleaseComObject(range);
+            Marshal.ReleaseComObject(worksheet);
+            Marshal.ReleaseComObject(workbook);
+            Marshal.ReleaseComObject(excelApp);
+
+            return result;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // 입력된 문자가 숫자나 제어 문자가 아닌 경우 입력을 막음
+            }
+        }
+    }
+}
